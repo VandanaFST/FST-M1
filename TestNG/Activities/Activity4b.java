b354f31d300fe0e314e798c7f5169a74f8bae918
VandanaFST <112411523+VandanaFST@users.noreply.github.com>
2022-11-29 20:49:41 +0530

    Create Activity4b.java

---
+++ TestNG/Activities/Activity4b.java
@@ -0,0 +1,9 @@
+package activities;
+
+import org.testng.annotations.Test;
+
+public class Activity4b {
+    @Test
+    public void TestCase() {
+        System.out.println("I'm in the test case from Activity4b Class");
+    }
